719c37ce26e066c882bca9a616835285105cc1c8
Patrícia <prrs89@gmail.com>
2019-07-15 18:57:51 +0100

    arrow

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,20 +1,24 @@
-function geo_success(position) {
+const geo_success = position => {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
+
+    let coordinates = {
+        latitude,
+        longitude
+    }
 
     console.log(latitude);
     console.log(longitude);
 
-    return { latitude, longitude };
+    return coordinates;
 }
 
-function geo_error(error) {
-    console.log('ERROR(' + error.code + '): ' + error.message);
+const geo_error = error => {
+    return `ERROR(${error.code}): ${error.message}`;
 }
 
 if (!navigator.geolocation) {
     console.log('Geolocation is not supported by your browser');
 } else {
     navigator.geolocation.getCurrentPosition(geo_success, geo_error);
-    console.log(geo_success());
 }

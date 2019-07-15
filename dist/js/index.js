02860891763259714604fc33ff280bfdf4e1aa26
Patrícia <prrs89@gmail.com>
2019-07-15 21:33:41 +0100

    back

--- dist/js/index.js
+++ dist/js/index.js
@@ -21,7 +21,6 @@ let deferredPrompt;
 const addBtn = document.querySelector('.add-button');
 
 window.addEventListener('beforeinstallprompt', (e) => {
-    console.log('here');
     // Prevent Chrome 67 and earlier from automatically showing the prompt
     e.preventDefault();
     // Stash the event so it can be triggered later.
@@ -45,3 +44,48 @@ window.addEventListener('beforeinstallprompt', (e) => {
         });
     });
 });
+
+// Determine if the app was successfully installed
+window.addEventListener('appinstalled', (evt) => {
+    console.log('a2hs installed');
+});
+
+// Detecting if your app is launched from the home screen
+// Safari
+if (window.navigator.standalone === true) {
+    console.log('display-mode is standalone');
+}
+// Other
+if (window.matchMedia('(display-mode: standalone)').matches) {
+    console.log('display-mode is standalone');
+}
+
+/* GEOLOCATION */
+
+const geo_success = position => {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    const coordinates = {
+        latitude,
+        longitude
+    }
+
+    console.log(coordinates);
+
+    return coordinates;
+}
+
+const geo_error = error => {
+    const errorString = `ERROR(${error.code}): ${error.message}`
+
+    console.log(errorString);
+
+    return errorString;
+}
+
+if (!navigator.geolocation) {
+    console.log('Geolocation is not supported by your browser');
+} else {
+    navigator.geolocation.getCurrentPosition(geo_success, geo_error);
+}

d27145d61cfff2f68ce69b7e005faa3e24da360c
Patrícia <prrs89@gmail.com>
2019-07-15 19:25:58 +0100

    add to home

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,5 @@
+/* GEOLOCATION */
+
 const geo_success = position => {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
@@ -24,4 +26,49 @@ if (!navigator.geolocation) {
     console.log('Geolocation is not supported by your browser');
 } else {
     navigator.geolocation.getCurrentPosition(geo_success, geo_error);
+}
+
+/* ADD TO HOME SCREEN */
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI notify the user they can add to home screen
+    btnAdd.style.display = 'block';
+});
+
+btnAdd.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    btnAdd.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice
+        .then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted the A2HS prompt');
+            } else {
+                console.log('User dismissed the A2HS prompt');
+            }
+            deferredPrompt = null;
+        });
+});
+
+// Determine if the app was successfully installed
+window.addEventListener('appinstalled', (evt) => {
+    console.log('a2hs installed');
+});
+
+// Detecting if your app is launched from the home screen
+// Safari
+if (window.navigator.standalone === true) {
+    console.log('display-mode is standalone');
+}
+// Other
+if (window.matchMedia('(display-mode: standalone)').matches) {
+    console.log('display-mode is standalone');
 }

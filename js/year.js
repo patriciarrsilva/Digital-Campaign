8cd4db8601203fd095ec6e7f35cb88c2f564b0c7
Patrícia <prrs89@gmail.com>
2019-07-20 19:00:58 +0100

    imgs

--- js/year.js
+++ js/year.js
@@ -2,6 +2,31 @@
 
 function autotab(current, to) {
     if (current.getAttribute && (current.value.length == current.getAttribute("maxlength"))) {
-        to.focus()
+        to.focus();
     }
 }
+
+const verifyBtn = document.getElementById("verify-button");
+
+verifyBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const one = document.getElementById("one").value;
+    const two = document.getElementById("two").value;
+    const three = document.getElementById("three").value;
+    const four = document.getElementById("four").value;
+
+    const year = one + two + three + four;
+    const d = new Date();
+    const currentYear = d.getFullYear();
+    const age = currentYear - year;
+
+    const oldestAge = 122;
+
+    if (age >= 18 && age <= oldestAge) {
+        window.location.href = "./html/location.html";
+    }
+
+    // in the real app, save the year to the backend
+    return year;
+});
